525caac6367b18a5bae0a5d965bbb89d06b7dd71
Diegomau182 <Diegomauri2013@gmail.com>
2020-03-13 16:54:36 -0600

    Creacion de los servicios de la funcion

---
+++ service_parking.py
@@ -0,0 +1,16 @@
+#-*-UTF-8-*-
+#Nombre:service_parking.py
+#objetivo:establece las funciones del parkeo 
+#Creador:Diego Diaz
+#Fecha:13/03/2020
+from cars import cars
+
+class service_parking:
+    """
+    Clase donde se encuentran todas los servicios 
+    y/o lo neceserio para del programa
+    """
+    def __init__(self):
+        """incializa todas los procesos"""
+        my_parking = list()
+        
