eee5d2c5b17f5d375014daf475d1c0125e1cc828
Diegomau182 <Diegomauri2013@gmail.com>
2020-03-13 16:53:48 -0600

    clase y constructor de la tupla

--- cars.py
+++ cars.py
@@ -3,5 +3,19 @@
 #objetivo:Establece la tupla donde se guarda la informacion del vehiculo
 #Creador:Diego Diaz
 #Fecha:13/03/2020
+from datetime import datetime, timedelta
 
 
+class cars:
+    """
+    inicializa un carro mediante la placa marca modelo
+    tipo_vehiculo, hora_ingreso, estado.
+    """
+    def __init__(self,placa="", marca="", modelo="", tipo_vehiculo=""):
+        self.placa=placa
+        self.marca=marca
+        self.modelo=modelo
+        self.tipo_vehiculo=tipo_vehiculo
+        self.hora_ingreso=datetime.time.today()
+        self.estado = True
+        self.plus_five_hours = (datetime.now() + timedelta(hours=5))
